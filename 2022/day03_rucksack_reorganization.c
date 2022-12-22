673b1862ce393aa9e4b928454707112d35e6898c
vcss7 <vs.victorsolis@gmail.com>
2022-12-22 09:10:24 -0800

    add day03_input.txt and complete part 1

--- 2022/day03_rucksack_reorganization.c
+++ 2022/day03_rucksack_reorganization.c
@@ -14,7 +14,7 @@
 
 void verify_args(int argc, char* argv[]);
 void usage(char* prog_name);
-char find_matching_char(char* str1, char* str2);
+char find_matching_char_in_halves(char* line);
 int get_priority_value(char ch);
 
 int main (int argc, char* argv[])
@@ -30,33 +30,64 @@ int main (int argc, char* argv[])
     }
 
     char line[64];
+    char comm_ch;
+    int sum = 0;
     while (fgets(line, 64, file_p) != NULL)
     {
         if (line[0] == '\n') // skip new lines
             continue;
 
         // one rucksack here
-        fprintf(stdout, "length: %ld\n", strlen(line));
         fputs(line, stdout);
 
         // split the string in half
-        // iterate through each character in string1 to see if it exist in string2
-        // if it exists, add appropriate prioty value to sum and continue to next rucksack
+        comm_ch = find_matching_char_in_halves(line);
+        fprintf(stdout, "comm_ch is %c\n", comm_ch);
+        fprintf(stdout, "value is %d\n", get_priority_value(comm_ch));
+
+        // assign priority value to char
+        sum += get_priority_value(comm_ch);
     }
 
     fclose(file_p);
 
+    fprintf(stdout, "Sum of priority values: %d\n", sum);
+
     return 0;
 }
 
-char find_matching_char(char* str1, char* str2)
+char find_matching_char_in_halves(char* line)
 {
+    char ch1;
+    char ch2;
+    size_t substr_size = strlen(line) / 2;
+
+    for (int i = 0; i < substr_size; i++)
+    {
+        ch1 = line[i];
+        for (int j = 0; j < substr_size; j++)
+        {
+            ch2 = line[j + substr_size];
+            if (ch1 == ch2)
+            {
+                return ch1;
+            }
+        }
+    }
+
     return 0;
 }
 
 int get_priority_value(char ch)
 {
-    return 0;
+    if (ch >= 'a')
+    {
+        return (ch - 'a' + 1);
+    }
+    else 
+    {
+        return (ch - 'A' + 26 + 1);
+    }
 }
 
 void verify_args(int argc, char* argv[])
