98b8fb145b49923e53156e4664e0165aadc9d9e2
vcss7 <vs.victorsolis@gmail.com>
2022-12-23 12:45:49 -0800

    add camp cleanup part 2

--- 2022/day04_camp_cleanup.c
+++ 2022/day04_camp_cleanup.c
@@ -8,11 +8,21 @@
  * Output:  The number of elves with assignments fully contained by their pairs
  */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct Elf
+{
+    int lower;
+    int upper;
+};
 
 void usage(char* prog_name);
 void verify_args(int argc, char* argv[]);
+bool is_enclosed(int lower1, int upper1, int lower2, int upper2);
+bool overlaps(int lower1, int upper1, int lower2, int upper2);
 
 int main(int argc, char* argv[])
 {
@@ -27,23 +37,63 @@ int main(int argc, char* argv[])
     }
 
     /*** part 1 ***/
-    char line1[64];
-    char line2[64];
+    char line1[32];
+    char line2[32];
+    struct Elf elf1;
+    struct Elf elf2;
 
-    while ((fgets(line1, 64, file_p)) != NULL)
+    int enclosed_sum = 0;
+    int overlap_sum = 0;
+    while ((fgets(line1, 32, file_p)) != NULL)
     {
         if (line1[0] == '\n')
             continue;;
 
-        fgets(line2, 64, file_p);
-
         fputs(line1, stdout);
-        fputs(line2, stdout);
+
+        strcpy(line1, strtok(line1, ","));
+        strcpy(line2, strtok(NULL, ","));
+
+        elf1.lower = atoi(strtok(line1, "-"));
+        elf1.upper = atoi(strtok(NULL, "-"));
+        fprintf(stdout, "elf1.lower > %d\n", elf1.lower);
+        fprintf(stdout, "elf1.upper > %d\n", elf1.upper);
+
+        elf2.lower = atoi(strtok(line2, "-"));
+        elf2.upper = atoi(strtok(NULL, "-"));
+        fprintf(stdout, "elf2.lower > %d\n", elf2.lower);
+        fprintf(stdout, "elf2.upper > %d\n", elf2.upper);
+
+        if (is_enclosed(elf1.lower, elf1.upper, elf2.lower, elf2.upper) ||
+            is_enclosed(elf2.lower, elf2.upper, elf1.lower, elf1.upper))
+        {
+            enclosed_sum++;
+        }
+        if (overlaps(elf1.lower, elf1.upper, elf2.lower, elf2.upper) ||
+            overlaps(elf2.lower, elf2.upper, elf1.lower, elf1.upper))
+        {
+            overlap_sum++;
+        }
     }
 
     fclose(file_p);
 
+    fprintf(stdout, "Total Enclosed > %d\n", enclosed_sum);
+    fprintf(stdout, "Total Overlapping > %d\n", overlap_sum);
+
     return 0;
+}
+
+bool overlaps(int lower1, int upper1, int lower2, int upper2)
+{
+    return ((lower1 <= lower2 && lower2 <= upper1)
+            || (lower1 <= upper2 && upper2 <= upper1));
+}
+
+bool is_enclosed(int lower1, int upper1, int lower2, int upper2)
+{
+    // test if bounds 2 is in bounds 1
+    return (lower1 <= lower2 && upper2<= upper1);
 }
 
 void verify_args(int argc, char* argv[])
@@ -59,7 +109,7 @@ void usage(char* prog_name)
 {
     fprintf(stderr, "usage is: %s <input_file>\n", prog_name);
     fprintf(stderr, "\tinput_file is file with pairs of elf assignments\n");
-    fprintf(stderr, "\teach line has assignemnts for an elf\n");
+    fprintf(stderr, "\teach line1 has assignemnts for an elf\n");
     fprintf(stderr, "\tsee: https://adventofcode.com/2022/day/4\n");
 }
 
