060e63d31594101908f472f90f31719da6dd9357
vcss7 <vs.victorsolis@gmail.com>
2025-10-18 18:01:27 -0700

    Update main.c template

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-const int MAX_LINE_LENGTH = 60;
+void part1(FILE *fptr);
+void part2(FILE *fptr);
 
 int main(void)
 {
@@ -19,20 +20,51 @@ int main(void)
         exit(1);
     }
 
-    char line[MAX_LINE_LENGTH];
+    part1(fptr);
 
-    while(fgets(line, sizeof(line), fptr))
-    {
-        if(ferror(fptr))
-        {
-            fprintf(stderr, "Error occured while reading file: %s\r\n", file_name);
-            exit(1);
-        }
+    rewind (fptr);
 
-        fprintf(stdout, "%s", line);
-    }
+    part2(fptr);
 
     fclose(fptr);
     return 0;
+}
+
+void part1(FILE *fptr)
+{
+    // solution for part one here
+    char ch;
+
+    while((ch = fgetc(fptr)) != EOF)
+    {
+        if(ferror(fptr))
+        {
+            fprintf(stderr, "Error occured while reading file\r\n");
+            exit(1);
+        }
+
+        fprintf(stdout, "%c", ch);
+    }
+
+    return;
+}
+
+void part2(FILE *fptr)
+{
+    // solution for part two here
+    char ch;
+
+    while((ch = fgetc(fptr)) != EOF)
+    {
+        if(ferror(fptr))
+        {
+            fprintf(stderr, "Error occured while reading file\r\n");
+            exit(1);
+        }
+
+        fprintf(stdout, "%c", ch);
+    }
+
+    return;
 }
 
