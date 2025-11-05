e4a1701544f9b9fb27497be6d91be21423e04bb9
vcss7 <vs.victorsolis@gmail.com>
2025-11-04 19:22:31 -0800

    Add file to day 7

---
+++ 2015/Day07/main.c
@@ -0,0 +1,59 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+uint16_t wireState[26][26];
+
+void part1 (const char* line, size_t len);
+void part2 (const char* line, size_t len);
+
+int main (void)
+{
+    FILE* fptr;
+    char* file_name = "input";
+
+    fptr = fopen (file_name, "r");
+
+    // check for valid file pointer
+    if (fptr == NULL)
+    {
+        fprintf (stderr, "Could not open file: %s\r\n", file_name);
+        exit (1);
+    }
+
+    // solution for part one here
+    char line[64];
+    size_t len = sizeof (line);
+
+    while (fgets (line, len, fptr))
+    {
+        if (ferror (fptr))
+        {
+            fprintf (stderr, "Error occured while reading file\r\n");
+            exit (1);
+        }
+
+        fprintf (stdout, "%s", line);
+
+        part1 (line, len);
+        part2 (line, len);
+    }
+
+    fclose (fptr);
+    return 0;
+}
+
+void part1 (const char* line, size_t len)
+{
+    // solution for part one here
+    
+
+    
+    return;
+}
+
+void part2 (const char* line, size_t len)
+{
+    // solution for part two here
+    return;
+}
